e8d127f0db4db4268397ba542324cc8ce23a21b3
weshell <weshell@foxmail.com>
2025-06-22 15:55:22 +0800

    speaker拆分

--- src/speaker.ts
+++ src/speaker.ts
@@ -12,14 +12,34 @@ const MOVE_TYPES = {
 };
 
 export function speak(move: IMove, board: IBoard) {
+    const finalSpeech = getChPGN(move, board);
+    const finalSpeechReplace = finalSpeech
+        .replace(/卒/g, '族') // "卒"（zú）常被读错为 cù
+        .replace(/將/g, '酱') // 繁体“將”可替代“将”，避免 jiāng
+        .replace(/将/g, '酱') // 简体“将”也处理
+        .replace(/相/g, '巷') // "相" 发 xiāng 时可能被误读
+        .replace(/仕/g, '市') // "仕"（shì）有时被读成 sī
+        .replace(/炮/g, '泡') // 部分语音引擎读成 bāo，替换为发音更接近的
+        .replace(/兵/g, '冰')
+        .replace(/傌/g, '马');
+    // 发音
+    if (!window.speechSynthesis) {
+        return;
+    }
+    const utter = new SpeechSynthesisUtterance(finalSpeechReplace);
+    utter.lang = 'zh-CN';
+    window.speechSynthesis.cancel(); // 停止前一条朗读
+    window.speechSynthesis.speak(utter);
+}
+export function getChPGN(move: IMove, board: IBoard): string {
     const { from, to } = move;
     const piece = board[from.x][from.y];
-    if (!piece) return;
+    if (!piece) return "";
 
     const isRed = piece === piece.toUpperCase();
     const pieceChar = PIECE_CHARS[piece];
     const numbers = isRed ? NUMBERS_RED : NUMBERS_BLACK;
-    const turn = isRed ? '红方' : '黑方';
+    const turn = isRed ? '红' : '黑';
     const BOARD: IBoard = Array.from({ length: 9 }, () => Array(10).fill(null));
     let fromx = from.x;
     let fromy = from.y;
@@ -88,23 +108,5 @@ export function speak(move: IMove, board: IBoard) {
         moveType = fromy < toy ? MOVE_TYPES.forward : MOVE_TYPES.backward;
         dest = numbers[tox];
     }
-
-    const finalSpeech = `${turn}:${pre}${moveType}${dest}`;
-    const finalSpeechReplace = finalSpeech
-        .replace(/卒/g, '族') // "卒"（zú）常被读错为 cù
-        .replace(/將/g, '酱') // 繁体“將”可替代“将”，避免 jiāng
-        .replace(/将/g, '酱') // 简体“将”也处理
-        .replace(/相/g, '巷') // "相" 发 xiāng 时可能被误读
-        .replace(/仕/g, '市') // "仕"（shì）有时被读成 sī
-        .replace(/炮/g, '泡') // 部分语音引擎读成 bāo，替换为发音更接近的
-        .replace(/兵/g, '冰')
-        .replace(/傌/g, '马');
-    // 发音
-    if (!window.speechSynthesis) {
-        return;
-    }
-    const utter = new SpeechSynthesisUtterance(finalSpeechReplace);
-    utter.lang = 'zh-CN';
-    window.speechSynthesis.cancel(); // 停止前一条朗读
-    window.speechSynthesis.speak(utter);
+    return `${turn}:${pre}${moveType}${dest}`;
 }
