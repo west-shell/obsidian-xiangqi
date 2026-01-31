c4879285998b4d0b9ac3dce64f63c074e09ebfa0
weshell <50977607+west-shell@users.noreply.github.com>
2026-01-31 18:06:57 +0800

    替换翻转图标

--- src/lib/Movelist/Toolbar.svelte
+++ src/lib/Movelist/Toolbar.svelte
@@ -25,7 +25,7 @@
     { title: "回退", icon: "arrow-left", event: "undo" },
     { title: "前进", icon: "arrow-right", event: "redo" },
     { title: "终局", icon: "arrow-right-to-line", event: "toEnd" },
-    { title: "翻转", icon: "rotate-ccw", event: "rotate" },
+    { title: "翻转", icon: "flip-vertical", event: "rotate" },
     { title: "皮卡鱼Web", icon: "external-link", event: "openPikafish" },
   ];
 
--- src/lib/Tree/Toolbar.svelte
+++ src/lib/Tree/Toolbar.svelte
@@ -14,7 +14,7 @@
     { title: "回退", icon: "arrow-left", event: "back" },
     { title: "前进", icon: "arrow-right", event: "next" },
     { title: "终局", icon: "arrow-right-to-line", event: "toEnd" },
-    { title: "翻转", icon: "rotate-ccw", event: "rotate" },
+    { title: "翻转", icon: "flip-vertical", event: "rotate" },
     { title: "皮卡鱼Web", icon: "external-link", event: "openPikafish" },
     { title: "标注", icon: "tag", event: "toggle-annotation-menu" },
   ];
@@ -63,10 +63,10 @@
         if (event === "toggle-annotation-menu") {
           handleAnnotationMenu(e); // ← 打开标注菜单
         } else if (event === "rotate") {
-    eventBus.emit("rotate");
-  } else {
-    emitEvent(event);
-  }
+          eventBus.emit("rotate");
+        } else {
+          emitEvent(event);
+        }
       }}
     ></button>
   {/each}
