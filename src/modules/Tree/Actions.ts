4fa3b32b8790a2639b4f6fe2fe12ad28763ed851
weshell <weshell@foxmail.com>
2026-08-11 20:10:50 +0800

    feat: add showMoveAnnotations toggle to engine settings modal

--- src/modules/Tree/Actions.ts
+++ src/modules/Tree/Actions.ts
@@ -477,6 +477,7 @@ const ActionsModule = {
       let skillValue = host.settings.engineSkillLevel;
       let showBestMove = host.settings.showEngineBestMove;
       let showPonder = host.settings.showEnginePonder;
+      let showAnnotations = host.settings.showMoveAnnotations;
 
       engineModal.onOpen = () => {
         const { contentEl } = engineModal;
@@ -534,6 +535,17 @@ const ActionsModule = {
           showPonder = ponderToggle.checked;
         });
 
+        // Show move annotations
+        const annContainer = contentEl.createDiv("engine-setting-toggle");
+        const annToggle = annContainer.createEl("input", { type: "checkbox" });
+        annToggle.checked = showAnnotations;
+        annContainer.createEl("label", {
+          text: t("engine.showMoveAnnotations"),
+        });
+        annToggle.addEventListener("change", () => {
+          showAnnotations = annToggle.checked;
+        });
+
         // Buttons
         const btnContainer = contentEl.createDiv("modal-button-container");
         const okBtn = btnContainer.createEl("button", {
@@ -549,6 +561,7 @@ const ActionsModule = {
           }
           host.settings.showEngineBestMove = showBestMove;
           host.settings.showEnginePonder = showPonder;
+          host.settings.showMoveAnnotations = showAnnotations;
           void host.plugin.saveSettings();
           host.plugin.refresh();
           engineModal.close();
