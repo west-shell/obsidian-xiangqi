116933d39e3800ffc6a9c3c9b3fc9d1b5c3b45e6
weshell <weshell@foxmail.com>
2026-08-16 14:44:16 +0800

    fix: remove grip-vertical icon from menu items

--- src/lib/Tree/Tree.svelte
+++ src/lib/Tree/Tree.svelte
@@ -104,7 +104,6 @@
       const label = `${i + 1}. ${white} ${result} ${black}${event ? ", " + event : ""}${date ? " " + date : ""}`;
       menu.addItem((mi) => {
         mi.setTitle(label)
-          .setIcon("grip-vertical")
           .setChecked(i === (currentGameIndex ?? 0))
           .onClick(() => eventBus.emit("switch-game", i));
       });
